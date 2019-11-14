4c34c1fee86c848bbbe54667c9db84a4c0bf3a45
Neil Watt <neil.watt@ymail.com>
2019-11-14 20:36:00 +0000

    added util and add_evidence function

--- StrategyLearner.py
+++ StrategyLearner.py
@@ -26,23 +26,35 @@ GT User ID: nwatt3 (replace with your User ID)
 GT ID: 903476861 (replace with your GT ID)  		   	  			  	 		  		  		    	 		 		   		 		  
 """  		   	  			  	 		  		  		    	 		 		   		 		  
   		   	  			  	 		  		  		    	 		 		   		 		  
-import datetime as dt  		   	  			  	 		  		  		    	 		 		   		 		  
-import pandas as pd  		   	  			  	 		  		  		    	 		 		   		 		  
-import util as ut  		   	  			  	 		  		  		    	 		 		   		 		  
+import datetime as dt  		   	  			  	 		  		  		    	 		 		   		 
+import pandas as pd  		   	  			  	 		  		  		    	 		 		   		 
+import util as ut  		   	  			  	 		  		  		    	 		 		   		 	
 import random  		   	  			  	 		  		  		    	 		 		   		 		  
+
+import BagLearner as bl
+#import QLearner as ql
+import DTLearner as dl
   		   	  			  	 		  		  		    	 		 		   		 		  
 class StrategyLearner(object):  		   	  			  	 		  		  		    	 		 		   		 		  
   		   	  			  	 		  		  		    	 		 		   		 		  
-    # constructor  		   	  			  	 		  		  		    	 		 		   		 		  
+      		   	  			  	 		  		  		    	 		 		   		 		  
     
     def author(self):
-        
-        
         return 'nwatt3'
     
+    # constructor
     def __init__(self, verbose = False, impact=0.0):  		   	  			  	 		  		  		    	 		 		   		 		  
-        self.verbose = verbose  		   	  			  	 		  		  		    	 		 		   		 		  
-        self.impact = impact  		   	  			  	 		  		  		    	 		 		   		 		  
+        self.verbose = verbose  		   	  			  	 		  		  		    	 
+        self.impact = impact  
+        self.window_size=20
+        self.feature_size = 5
+        self.N = 10
+        bag=20
+        leaf_size = 5
+        self.learner=bl.BagLearner(learner=dl.DTLearner, bags=bag, kwargs={"leaf_size":leaf_size})
+        
+        
+        
   		   	  			  	 		  		  		    	 		 		   		 		  
     # this method should create a QLearner, and train it for trading  		   	  			  	 		  		  		    	 		 		   		 		  
     def addEvidence(self, symbol = "IBM", \
@@ -51,19 +63,66 @@ class StrategyLearner(object):
         sv = 10000):  		   	  			  	 		  		  		    	 		 		   		 		  
   		   	  			  	 		  		  		    	 		 		   		 		  
         # add your code to do learning here  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
+  		window_size=self.window_size
+        feature_size = self.feature_size
+        N = self.N
+        impact=self.impact
+        threshold = max(0.05, 2 * impact)
+        
         # example usage of the old backward compatible util function  		   	  			  	 		  		  		    	 		 		   		 		  
-        syms=[symbol]  		   	  			  	 		  		  		    	 		 		   		 		  
-        dates = pd.date_range(sd, ed)  		   	  			  	 		  		  		    	 		 		   		 		  
-        prices_all = ut.get_data(syms, dates)  # automatically adds SPY  		   	  			  	 		  		  		    	 		 		   		 		  
-        prices = prices_all[syms]  # only portfolio symbols  		   	  			  	 		  		  		    	 		 		   		 		  
-        prices_SPY = prices_all['SPY']  # only SPY, for comparison later  		   	  			  	 		  		  		    	 		 		   		 		  
-        if self.verbose: print(prices)  		   	  			  	 		  		  		    	 		 		   		 		  
+        syms=[symbol]  		   	  			  	 		  		  		    	 		 		   		 
+        dates = pd.date_range(sd, ed)  		   	  			  	 		  		  		    	 		 
+        prices_all = ut.get_data(syms, dates)  # automatically adds SPY  		   	  			  	 	 
+        prices = prices_all[syms]  # only portfolio symbols  		   	  			  	 		  
+        prices_SPY = prices_all['SPY']  # only SPY, for comparison later  		   	  			  	 	
+        norm_prices=prices.divide(prices.ix[0])
+        
+        
+        #if self.verbose: print(prices)  		   	  			  	 		  		  		    	 		 		  
+        
+        # Add My Indicators: SMA,BB,Momentum
+        #1. SMA:
+        smap=norm_prices.copy()
+        smap['SMA/P']=prices.rolling(window_size).mean()/prices
+
+        #2. BB: Bollinger Band Index
+        bb=norm_prices.copy()
+        bb['SMA']=norm_prices.rolling(window_size).mean()
+        bb['STD']=norm_prices.rolling(window_size).std()
+        bb['Upper BB']=bb['SMA']+2.0*bb['STD']
+        bb['Lower BB']=bb['SMA']-2.0*bb['STD']
+        bb['BBI']=(bb.ix[:, 0]-bb['Lower BB'])/(bb['Upper BB']-bb['Lower BB'])
+
+        #3. MM: Momentum
+        MM = norm_prices.copy()
+        MM['Momentum'] = MM.divide(MM.shift(window_size)) - 1
   		   	  			  	 		  		  		    	 		 		   		 		  
         # example use with new colname  		   	  			  	 		  		  		    	 		 		   		 		  
-        volume_all = ut.get_data(syms, dates, colname = "Volume")  # automatically adds SPY  		   	  			  	 		  		  		    	 		 		   		 		  
-        volume = volume_all[syms]  # only portfolio symbols  		   	  			  	 		  		  		    	 		 		   		 		  
-        volume_SPY = volume_all['SPY']  # only SPY, for comparison later  		   	  			  	 		  		  		    	 		 		   		 		  
+        #volume_all = ut.get_data(syms, dates, colname = "Volume")  # automatically adds SPY  		   	 
+        #volume = volume_all[syms]  # only portfolio symbols  		   	  			  	 		  		 
+        #volume_SPY = volume_all['SPY']  # only SPY, for comparison later  		   	  			  	 	
+        X_train=[]
+        Y_train=[]
+
+        for i in range(window_size + feature_size + 1, len(prices) - N):
+            X_train.append( np.concatenate( (smap['SMA/P'][i - feature_size : i], bb['BBI'][i - feature_size : i], MM['Momentum'][i - feature_size : i]) ) )
+            ret= (prices.values[i + N] - prices.values[i]) / prices.values[i]
+            #Cal. N days return
+            if ret > threshold:
+                Y_train.append(1)
+            elif ret < -threshold:
+                Y_train.append(-1)
+            else:
+                Y_train.append(0)
+
+        X_train=np.array(X_train)
+        Y_train=np.array(Y_train)
+
+        self.learner.addEvidence(X_train, Y_train)
+        
+        
+        
+        
         if self.verbose: print(volume)  		   	  			  	 		  		  		    	 		 		   		 		  
   		   	  			  	 		  		  		    	 		 		   		 		  
     # this method should use the existing policy and test it against new data  		   	  			  	 		  		  		    	 		 		   		 		  
