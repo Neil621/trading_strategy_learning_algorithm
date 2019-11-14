50a9166d97bcbeb386133383340c8f5ff8594879
Neil Watt <neil.watt@ymail.com>
2019-11-14 22:57:29 +0000

    adding strategy

--- StrategyLearner.py
+++ StrategyLearner.py
@@ -24,63 +24,53 @@ GT honor code violation.
 Student Name: Neil Watt (replace with your name)  		   	  			  	 		  		  		    	 		 		   		 		  
 GT User ID: nwatt3 (replace with your User ID)  		   	  			  	 		  		  		    	 		 		   		 		  
 GT ID: 903476861 (replace with your GT ID)  		   	  			  	 		  		  		    	 		 		   		 		  
-"""  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-import datetime as dt  		   	  			  	 		  		  		    	 		 		   		 
-import pandas as pd  		   	  			  	 		  		  		    	 		 		   		 
-import util as ut  		   	  			  	 		  		  		    	 		 		   		 	
-import random  
-import numpy as np
-
+"""  		   	  			  	 		  		  		    	 		 		   	
+import datetime as dt
+import pandas as pd
+import util as ut
 import BagLearner as bl
 #import QLearner as ql
 import DTLearner as dl
-  		   	  			  	 		  		  		    	 		 		   		 		  
-class StrategyLearner(object):  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-      		   	  			  	 		  		  		    	 		 		   		 		  
-    
-    def author(self):
-        return 'nwatt3'
-    
+import numpy as np
+import random
+
+class StrategyLearner(object):
+
     # constructor
-    def __init__(self, verbose = False, impact=0.0):  		   	  			  	 		  		  		    	 		 		   		 		  
-        self.verbose = verbose  		   	  			  	 		  		  		    	 
-        self.impact = impact  
+    def __init__(self, verbose = False, impact=0.0):
+        self.verbose = verbose
+        self.impact = impact
         self.window_size=20
         self.feature_size = 5
         self.N = 10
         bag=20
         leaf_size = 5
         self.learner=bl.BagLearner(learner=dl.DTLearner, bags=bag, kwargs={"leaf_size":leaf_size})
-        
-        
-        
-  		   	  			  	 		  		  		    	 		 		   		 		  
-    # this method should create a QLearner, and train it for trading  		   	  			  	 		  		  		    	 		 		   		 		  
+
+    def author(self):
+        return 'nwatt'
+
+    # this method should create a QLearner, and train it for trading
     def addEvidence(self, symbol = "IBM", \
         sd=dt.datetime(2008,1,1), \
-        ed=dt.datetime(2009,1,1),sv = 10000):  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-        # add your code to do learning here  		   	  			  	 		  		  		    	 
-        
+        ed=dt.datetime(2009,1,1), \
+        sv = 10000): 
+
+        # add your code to do learning here
         window_size=self.window_size
         feature_size = self.feature_size
         N = self.N
         impact=self.impact
         threshold = max(0.05, 2 * impact)
-        
-        # example usage of the old backward compatible util function  		   	  			  	 		  		  		    	 		 		   		 		  
-        syms=[symbol]  		   	  			  	 		  		  		    	 		 		   		 
-        dates = pd.date_range(sd, ed)  		   	  			  	 		  		  		    	 		 
-        prices_all = ut.get_data(syms, dates)  # automatically adds SPY  		   	  			  	 	 
-        prices = prices_all[syms]  # only portfolio symbols  		   	  			  	 		  
-        prices_SPY = prices_all['SPY']  # only SPY, for comparison later  		   	  			  	 	
+        # example usage of the old backward compatible util function
+        syms=[symbol]
+        dates = pd.date_range(sd, ed)
+        prices_all = ut.get_data(syms, dates)  # automatically adds SPY
+        prices = prices_all[syms]  # only portfolio symbols
         norm_prices=prices.divide(prices.ix[0])
-        
-        
-        #if self.verbose: print(prices)  		   	  			  	 		  		  		    	 		 		  
-        
+        #prices_SPY = prices_all['SPY']  # only SPY, for comparison later
+        #if self.verbose: print prices
+  
         # Add My Indicators: SMA,BB,Momentum
         #1. SMA:
         smap=norm_prices.copy()
@@ -97,11 +87,12 @@ class StrategyLearner(object):
         #3. MM: Momentum
         MM = norm_prices.copy()
         MM['Momentum'] = MM.divide(MM.shift(window_size)) - 1
-  		   	  			  	 		  		  		    	 		 		   		 		  
-        # example use with new colname  		   	  			  	 		  		  		    	 		 		   		 		  
-        #volume_all = ut.get_data(syms, dates, colname = "Volume")  # automatically adds SPY  		   	 
-        #volume = volume_all[syms]  # only portfolio symbols  		   	  			  	 		  		 
-        #volume_SPY = volume_all['SPY']  # only SPY, for comparison later  		   	  			  	 	
+
+        #Normalization
+        # smap['SMA/P'] = (smap['SMA/P']-smap['SMA/P'].mean())/smap['SMA/P'].std()
+        # bb['BBI'] = (bb['BBI']-bb['BBI'].mean())/bb['BBI'].std()
+        # MM['Momentum'] = (MM['Momentum']-MM['Momentum'].mean())/MM['Momentum'].std()
+        
         X_train=[]
         Y_train=[]
 
@@ -120,37 +111,31 @@ class StrategyLearner(object):
         Y_train=np.array(Y_train)
 
         self.learner.addEvidence(X_train, Y_train)
-        
-        
-        
-        
-        if self.verbose: print(volume)  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-    # this method should use the existing policy and test it against new data  		   	  			  	 		  		  		    	 		 		   		 		  
+    # this method should use the existing policy and test it against new data
     def testPolicy(self, symbol = "IBM", \
         sd=dt.datetime(2009,1,1), \
         ed=dt.datetime(2010,1,1), \
-        sv = 10000):  		   	  			  	 		  		  		    	 		 		   		 		  
+        sv = 10000):
+
         current_holding=0
-  		   	  			  	 		  		  		    	 		 		   		 		  
-        # here we build a fake set of trades  		   	  			  	 		  		  		    	 		 		   		 		  
-        # your code should return the same sort of data  		   	  			  	 		  		  		    	 		 		   		 		  
-        dates = pd.date_range(sd, ed)  		   	  			  	 		  		  		    	 		 
-        prices_all = ut.get_data([symbol], dates)  # automatically adds SPY  		   	  			  	 
-        trades = prices_all[[symbol,]]  # only portfolio symbols  		   	  			  	 		  	
-        trades_SPY = prices_all['SPY']  # only SPY, for comparison later  		   	  			  	 	 
+
+        # here we build a fake set of trades
+        # your code should return the same sort of data
+        dates = pd.date_range(sd, ed)
+        prices_all = ut.get_data([symbol], dates)  # automatically adds SPY
+        trades = prices_all[[symbol,]]  # only portfolio symbols
+        trades_SPY = prices_all['SPY']  # only SPY, for comparison later
+
+        window_size=self.window_size
+        feature_size = self.feature_size
+        N=self.N
+
+        syms=[symbol]
+        dates = pd.date_range(sd, ed)
+        prices_all = ut.get_data(syms, dates)  # automatically adds SPY
         prices = prices_all[syms]  # only portfolio symbols
         norm_prices=prices.divide(prices.ix[0])
-        
-        
-        #trades.values[:,:] = 0 # set them all to nothing  		   	  			  	 		  		  	
-        #trades.values[0,:] = 1000 # add a BUY at the start  		   	  			  	 		  		 
-        #trades.values[40,:] = -1000 # add a SELL  		   	  			  	 		  		  		     
-        #trades.values[41,:] = 1000 # add a BUY  		   	  			  	 		  		  		    
-        #trades.values[60,:] = -2000 # go short from long  		   	  			  	 		  		  	
-        #trades.values[61,:] = 2000 # go long from short  		   	  			  	 		  		  	
-        #trades.values[-1,:] = -1000 #exit on the last day  		   	  			  	 		  		  	  
-        
+
         # Add My Indicators: SMA,BB,Momentum
         #1. SMA:
         smap=norm_prices.copy()
@@ -187,11 +172,10 @@ class StrategyLearner(object):
                 trades.values[i + window_size + feature_size + 1, :] = - 1000 - current_holding
                 current_holding = -1000
 
-        
-        if self.verbose: print(type(trades)) # it better be a DataFrame!  		   	  			  	 		  		  		    	 		 		   		 		  
-        if self.verbose: print(trades)  		   	  			  	 		  		  		    	 		 		   		 		  
-        if self.verbose: print(prices_all)  		   	  			  	 		  		  		    	 		 		   		 		  
-        return trades  		   	  			  	 		  		  		    	 		 		   		 		  
-  		   	  			  	 		  		  		    	 		 		   		 		  
-if __name__=="__main__":  		   	  			  	 		  		  		    	 		 		   		 		  
-    print("One does not simply think up a strategy")  		   	  			  	 		  		  		    	 		 		   		 		  
+        if self.verbose: print(type(trades)) # it better be a DataFrame!
+        if self.verbose: print(trades)
+        if self.verbose: print(prices_all)
+        return trades
+
+if __name__=="__main__":
+    print("One does not simply think up a strategy")
